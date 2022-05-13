d0186cf434d57e13b91b7f020e8daae9310fae8b
Martin Guillon <martin.guillon@akylas.fr>
2022-05-13 22:08:56 +0200

    fix(android): multiple bug fixes and improvements

--- src/ui-range-seek-bar/angular/range-seek-bar.module.ts
+++ src/ui-range-seek-bar/angular/range-seek-bar.module.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { registerElement } from '@nativescript/angular';
-registerElement('RangeSeekBar', () => require('../').RangeSeekBar);
+import { RangeSeekBar } from '@nativescript-community/ui-range-seek-bar';
+registerElement('RangeSeekBar', () => RangeSeekBar);
 
 @NgModule({
     schemas: [NO_ERRORS_SCHEMA]
